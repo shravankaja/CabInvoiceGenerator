26447a371705d890d922742a2cb110cd731e0291
shravan-kaja <shravan.kaja@gmail.com>
2021-03-14 06:48:19 +0530

    Added: test case to check if correct fare is returned on multiple rides

--- src/test/java/com/cabinvoiceggenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/cabinvoiceggenerator/InvoiceGeneratorTest.java
@@ -3,11 +3,25 @@ package com.cabinvoiceggenerator;
 import org.junit.jupiter.api.*;
 
 public class InvoiceGeneratorTest {
+    InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+
     @Test
     void givenDistanceAndTimeShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator=new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
-        Assertions.assertEquals(25,invoiceGenerator.calculateFare(distance,time));
+        Assertions.assertEquals(25, invoiceGenerator.calculateFare(distance, time), 0.0);
+    }
+
+    @Test
+    void givenDistanceAndTimeShouldReturnMinimumFareTotalFare() {
+        double distance = 0.1;
+        int time = 2;
+        Assertions.assertEquals(5, invoiceGenerator.calculateFare(distance, time), 0.0);
+    }
+
+    @Test
+    void givenMultipleRidesShouldReturnTotalCost() {
+        Ride[] rides = {new Ride(2, 5), new Ride(5, 6)};
+        Assertions.assertEquals(81, invoiceGenerator.calculateFareForAllRides(rides), 0.0);
     }
 }
