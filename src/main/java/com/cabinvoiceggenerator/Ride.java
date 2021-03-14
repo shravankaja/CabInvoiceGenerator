b036043361b194cdbef7513c630959f20256892e
shravan-kaja <shravan.kaja@gmail.com>
2021-03-14 08:34:21 +0530

    Added: userId to every Ride for processing

--- src/main/java/com/cabinvoiceggenerator/Ride.java
+++ src/main/java/com/cabinvoiceggenerator/Ride.java
@@ -3,9 +3,24 @@ package com.cabinvoiceggenerator;
 public class Ride {
     double distance;
     int time;
+    int userId;
 
-    public Ride(double distance, int time) {
+    public int getUserId() {
+        return userId;
+    }
+
+    public Ride(double distance, int time, int userId) {
         this.distance = distance;
         this.time = time;
+        this.userId=userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Ride{" +
+                "distance=" + distance +
+                ", time=" + time +
+                ", userId=" + userId +
+                '}';
     }
 }
