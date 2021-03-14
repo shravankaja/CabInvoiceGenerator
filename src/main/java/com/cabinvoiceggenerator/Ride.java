8e0841dd257049cc7fe254315fd10ade00d2a84d
shravan-kaja <shravan.kaja@gmail.com>
2021-03-14 06:46:46 +0530

    Added: defined Ride class to set distance and time for each ride

---
+++ src/main/java/com/cabinvoiceggenerator/Ride.java
@@ -0,0 +1,11 @@
+package com.cabinvoiceggenerator;
+
+public class Ride {
+    double distance;
+    int time;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
