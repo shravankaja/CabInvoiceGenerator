f8a79f69767a068681a38c01cecc8e2b266bedf1
shravan-kaja <shravan.kaja@gmail.com>
2021-03-14 09:32:17 +0530

    Refactored: all the methods to accomidate ride type

--- src/main/java/com/cabinvoiceggenerator/InvoiceGenerator.java
+++ src/main/java/com/cabinvoiceggenerator/InvoiceGenerator.java
@@ -8,22 +8,47 @@ import java.util.*;
 
 public class InvoiceGenerator {
 
-    private static final double MINIMUM_COST_PER_KILOMETER = 10;
-    private static final int COST_PER_TIME = 1;
-    private static final int MINIMUM_FARE = 5;
+    private static final double MINIMUM_COST_PER_KILOMETER_REGULAR = 10;
+    private static final int COST_PER_TIME_REGULAR = 1;
+    private static final int MINIMUM_FARE_REGULAR = 5;
+    private static final double MINIMUM_COST_PER_KILOMETER_PREMIUM = 15;
+    private static final int COST_PER_TIME_PREMIUM = 2;
+    private static final int MINIMUM_FARE_PREMIUM = 20;
 
+    public double calculateFare(double distance, int time, RideType type) {
+        double totalFareFinal = 0;
+        switch (type) {
 
-    public double calculateFare(double distance, int time) {
-        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
-        if (totalFare < MINIMUM_FARE) return MINIMUM_FARE;
-        return totalFare;
+            case REGULAR:
+                double totalFare = distance * MINIMUM_COST_PER_KILOMETER_REGULAR + time * COST_PER_TIME_REGULAR;
+                if (totalFare < MINIMUM_FARE_REGULAR) return MINIMUM_FARE_REGULAR;
+                totalFareFinal = totalFare;
+                break;
+            case PREMIUM:
+                totalFare = distance * MINIMUM_COST_PER_KILOMETER_PREMIUM + time * COST_PER_TIME_PREMIUM;
+                if (totalFare < MINIMUM_FARE_PREMIUM) return MINIMUM_FARE_PREMIUM;
+                totalFareFinal = totalFare;
+                break;
+
+        }
+        return totalFareFinal;
     }
 
     public double calculateFareForAllRides(Ride[] rides) {
         double totalFare = 0;
+        double totalFarePremium = 0;
+        double totalFareRegular = 0;
         for (Ride ride : rides) {
-            totalFare += this.calculateFare(ride.distance, ride.time);
+            if (ride.type.equals(RideType.PREMIUM)) {
+                totalFarePremium += this.calculateFare(ride.distance, ride.time, RideType.PREMIUM);
+            } else if (ride.type.equals(RideType.REGULAR)) {
+                totalFareRegular += this.calculateFare(ride.distance, ride.time, RideType.REGULAR);
+            }
+
         }
+        System.out.println("Total fare for Regular rides:" + totalFareRegular);
+        System.out.println("Total fare for Premimum rides:" + totalFarePremium);
+        totalFare = totalFarePremium + totalFareRegular;
         return totalFare;
     }
 
@@ -56,6 +81,8 @@ public class InvoiceGenerator {
                     appendStrToFile(String.valueOf(filePath), String.valueOf(ride.time));
                     appendStrToFile(String.valueOf(filePath), ",");
                     appendStrToFile(String.valueOf(filePath), String.valueOf(ride.userId));
+                    appendStrToFile(String.valueOf(filePath), ",");
+                    appendStrToFile(String.valueOf(filePath), String.valueOf(ride.type));
                     appendStrToFile(String.valueOf(filePath), "\n");
                 } else if (Files.exists(filePath)) {
                     appendStrToFile(String.valueOf(filePath), String.valueOf(ride.distance));
@@ -63,9 +90,21 @@ public class InvoiceGenerator {
                     appendStrToFile(String.valueOf(filePath), String.valueOf(ride.time));
                     appendStrToFile(String.valueOf(filePath), ",");
                     appendStrToFile(String.valueOf(filePath), String.valueOf(ride.userId));
+                    appendStrToFile(String.valueOf(filePath), ",");
+                    appendStrToFile(String.valueOf(filePath), String.valueOf(ride.type));
                     appendStrToFile(String.valueOf(filePath), "\n");
                 }
             }
+        }
+    }
+
+    public static void appendStrToFile(String fileName, String str) {
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(fileName, true));
+            out.write(str);
+            out.close();
+        } catch (IOException e) {
+            System.out.println("exception occoured" + e);
         }
     }
 
@@ -84,17 +123,22 @@ public class InvoiceGenerator {
                 CSVReader reader = new CSVReader(new FileReader(String.valueOf(filePath)));
                 String[] nextLine;
                 while ((nextLine = reader.readNext()) != null) {
-
                     System.out.print("Distance :" + nextLine[0]);
                     System.out.println("\n");
                     System.out.print("Time :" + nextLine[1]);
                     System.out.println("\n");
                     System.out.print("User Id :" + nextLine[2]);
                     System.out.println("\n");
+                    System.out.print("Ride type :" + nextLine[3]);
                     System.out.println("--------------------------------");
                     count = count + 1;
-                    rideArrayList.add(new Ride(Double.parseDouble(nextLine[0]), Integer.parseInt(nextLine[1]),
-                            Integer.parseInt(nextLine[2])));
+                    if (nextLine[3].equals("REGULAR")) {
+                        rideArrayList.add(new Ride(Double.parseDouble(nextLine[0]), Integer.parseInt(nextLine[1]),
+                                Integer.parseInt(nextLine[2]), RideType.REGULAR));
+                    } else if (nextLine[3].equals("PREMIUM")) {
+                        rideArrayList.add(new Ride(Double.parseDouble(nextLine[0]), Integer.parseInt(nextLine[1]),
+                                Integer.parseInt(nextLine[2]), RideType.PREMIUM));
+                    }
                 }
             }
         }
@@ -102,16 +146,6 @@ public class InvoiceGenerator {
         return count;
     }
 
-
-    public static void appendStrToFile(String fileName, String str) {
-        try {
-            BufferedWriter out = new BufferedWriter(new FileWriter(fileName, true));
-            out.write(str);
-            out.close();
-        } catch (IOException e) {
-            System.out.println("exception occoured" + e);
-        }
-    }
 
     public double[] enhancedInvoiceList(ArrayList<Ride> rides) throws IOException {
         double totalFare = this.calculateFareForAllRidesList(rides);
@@ -126,9 +160,21 @@ public class InvoiceGenerator {
 
     public double calculateFareForAllRidesList(ArrayList<Ride> rides) {
         double totalFare = 0;
+        double totalFarePremium = 0;
+        double totalFareRegular = 0;
         for (Ride ride : rides) {
-            totalFare += this.calculateFare(ride.distance, ride.time);
+            if (ride.type.equals(RideType.PREMIUM)) {
+                totalFarePremium += this.calculateFare(ride.distance, ride.time, RideType.PREMIUM);
+            } else if (ride.type.equals(RideType.REGULAR)) {
+                totalFareRegular += this.calculateFare(ride.distance, ride.time, RideType.REGULAR);
+            }
         }
+        System.out.println("Total fare for Regular rides:" + totalFareRegular);
+        System.out.println("Total fare for Premimum rides:" + totalFarePremium);
+        totalFare = totalFarePremium + totalFareRegular;
         return totalFare;
     }
 }
+
+
+
